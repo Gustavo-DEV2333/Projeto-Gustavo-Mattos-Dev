18d5f606511a63febda9f494041144bbd6016020
Gustavo Mattos <gustavo.mattosbk98@gmail.com>
2026-06-09 23:07:05 -0300

    corrige caminhos das imagens no modo claro

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const img = document.querySelector("#profile img");
 //substituir a imagem
 if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-  img.setAttribute("src", "./assets/Avatar-light.png");
+  img.setAttribute("src", "assets/avatar-light.png");
 } else {
     //se tiver sem light mode, manter a imagem normal
-  img.setAttribute("src", "./assets/Avatar.png");
+  img.setAttribute("src", "assets/Avatar.png");
 }
 
 
